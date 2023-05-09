7a0a294249007c3fd1d863ce446504426964a737
Joao Costa <joaocostagr@outlook.pt>
2023-05-09 01:30:48 +0100

    changing financis/financis to financis

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+// Rota para Dividas
+Route::get('/dividas', [App\Http\Controllers\HomeController::class, 'dividas'])->name('dividas');
+Route::post('/dividas', [App\Http\Controllers\HomeController::class, 'inserirDividas'])->name('dividas');
+// Rota para Poupancas
+Route::get('/poupancas', [App\Http\Controllers\HomeController::class, 'poupancas'])->name('poupancas');
+Route::post('/poupancas', [App\Http\Controllers\HomeController::class, 'inserirPoupancas'])->name('poupancas');
+Route::get('/', function () {
+    return view('home');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
